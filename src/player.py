c0d603fd71394945a1e1eb867dcce36dce470f20
Justin A <jarata@gmail.com>
2019-10-03 20:47:11 -0700

    additem Item class adding items array to room and player

--- src/player.py
+++ src/player.py
@@ -3,6 +3,11 @@
 class Player:
     def __init__(self, room):
         self.room = room
+        self.items = []
         
     def __repr__(self):
         return f"Player is currently in {self.room}"
+    
+    
+# sq brackets for objects
+# parens for functions
--- src/room.py
+++ src/room.py
@@ -1,10 +1,11 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
 class Room:
-    def __init__(self, name, description):
+    def __init__(self, name, description, items):
         self.name = name
         self.description = description
+        self.items = []
     
     def __repr__(self):
-        return f"self.name + self.description"
+        return f"{self.name}: {self.description}"
     
